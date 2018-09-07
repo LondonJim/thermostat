dd5cee0817b6dfefb7aa1b484df4e2dc020843fc
James Sutherland <jrsutherland78@googlemail.com>
2018-09-07 14:13:46 +0100

    add css

--- src/localWeather.js
+++ src/localWeather.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
   $.get('http://api.openweathermap.org/data/2.5/weather?q=London,uk&units=metric&appid=a3d9eb01d4de82b9b8d0849ef604dbed', function (data) {
-    $('#outsideTemperature').text(data.main.temp);
+    $('#outsideTemperature').text(Math.round(data.main.temp));
     $('#localCity').text(data.name);
   });
 });
